d4c2367cf2c6f3c869bfc29c7652283ce725ebdc
hizas <hizassr@gmail.com>
2016-03-13 01:44:11 +0700

    Initialize Class

---
+++ TubesPBO/src/Admin.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+
+public class Admin {
+    private ArrayList<Gerbong> daftarGerbong = new ArrayList<>();
+    private ArrayList<Kereta> daftarKereta = new ArrayList<>();
+    private ArrayList<Rute> daftarRute = new ArrayList<>();
+    private ArrayList<Stasiun> daftarStasiun = new ArrayList<>();
+    private ArrayList<Tiket> daftarTiket = new ArrayList<>();
+    
+    public Gerbong getGerbong(String id)
+    {
+        for(Gerbong x : daftarGerbong)
+        {
+            if(x.getIdGerbong().equals(id))
+                return x;
+        }
+        return null;
+    }
+    public Kereta getKereta(String id)
+    {
+        for(Kereta x : daftarKereta)
+        {
+            if(x.getIdKereta().equals(id))
+                return x;
+        }
+        return null;
+    }
+    public Rute getRute(String id)
+    {
+        for(Rute x : daftarRute)
+        {
+            if(x.getIdRute().equals(id))
+                return x;
+        }
+        return null;
+    }
+    public Stasiun getStasiun(String id)
+    {
+        for(Stasiun x : daftarStasiun)
+        {
+            if(x.getIdStasiun().equals(id))
+                return x;
+        }
+        return null;
+    }
+    public Tiket getTiket(String id)
+    {
+        for(Tiket x : daftarTiket)
+        {
+            if(x.getIdTiket().equals(id))
+                return x;
+        }
+        return null;
+    }
+    
+    public void addGerbong(Gerbong gerbong) { daftarGerbong.add(gerbong); }
+    public void addKereta(Kereta kereta) { daftarKereta.add(kereta); }
+    public void addRute(Rute rute) { daftarRute.add(rute); }
+    public void addStasiun(Stasiun stasiun) { daftarStasiun.add(stasiun); }
+    public void addTiket(Tiket tiket) { daftarTiket.add(tiket); }
+    
+    public void removeGerbong(String id) 
+    {
+        for(Gerbong p : daftarGerbong)
+        {
+            if(p.getIdGerbong().equals(id))
+            {
+                daftarGerbong.remove(p);
+                break;
+            }
+        }
+    }
+    public void removeKereta(String id) 
+    {
+        for(Kereta p : daftarKereta)
+        {
+            if(p.getIdKereta().equals(id))
+            {
+                daftarKereta.remove(p);
+                break;
+            }
+        }
+    }
+    public void removeStasiun(String id) 
+    {
+        for(Stasiun p : daftarStasiun)
+        {
+            if(p.getIdStasiun().equals(id))
+            {
+                daftarStasiun.remove(p);
+                break;
+            }
+        }
+    }
+    public void removeTiket(String id) 
+    {
+        for(Tiket p : daftarTiket)
+        {
+            if(p.getIdTiket().equals(id))
+            {
+                daftarTiket.remove(p);
+                break;
+            }
+        }
+    }
+    public void removeRute(String id) 
+    {
+        for(Rute p : daftarRute)
+        {
+            if(p.getIdRute().equals(id))
+            {
+                daftarRute.remove(p);
+                break;
+            }
+        }
+    }
+}
--- TubesPBO/src/Kereta.java
+++ TubesPBO/src/Kereta.java
@@ -1,129 +1,72 @@
 public class Kereta {
-	//atribut
+    //Atribut
     private String idKereta;
     private String namaKereta;
     private Gerbong[] daftarGerbong;
-    private Rute[] daftarRute;
     private int kapasitasGerbong;
-    private int kapasitasRute;
     private int jumlahGerbong = 0;
-    private int jumlahRute = 0;
     
-	//constructor
-    public Kereta(String id, String nama, int maxGerbong, int maxRute){
-		idKereta = id;
-		namaKereta = nama;
-		kapasitasGerbong = maxGerbong;
-		kapasitasRute = maxRute;
-		daftarGerbong = new Gerbong[maxGerbong];
-		daftarRute = new Rute[maxRute];
-	}
+    //Constructor
+    public Kereta(String id, String nama, int maxGerbong){
+	idKereta = id;
+	namaKereta = nama;
+	kapasitasGerbong = maxGerbong;
+	daftarGerbong = new Gerbong[maxGerbong];
+    }
+    
     //Getter
-	public String getIdKereta() { 
-		return idKereta; 
+    public String getIdKereta() { 
+	return idKereta; 
+    }
+    public String getNamaKereta() { 
+	return namaKereta; 
+    }
+    public Gerbong getGerbong(String id) { 
+	for(int i = 0; i < kapasitasGerbong; i++){
+            if(daftarGerbong[i].getIdGerbong().equals(id)){
+                return daftarGerbong[i];
+            }	
 	}
-	
-	public String getNamaKereta() { 
-		return namaKereta; 
+	return null;
+    }
+    public int getKapasitasGerbong() { 
+        return kapasitasGerbong; 
+    }	
+    public int getJumlahGerbong() { 
+	return jumlahGerbong;
+    }
+    
+    //Setter
+    public void setIdKereta(String id) {
+        idKereta = id; 
+    }
+    public void setNamaKereta(String nama) {
+        namaKereta = nama; 
+    }	
+    public void addGerbong(Gerbong gerbong) { 
+        boolean success = false;
+        for(int i = 0; i < kapasitasGerbong; i++) {
+            if(daftarGerbong[i] == null) {
+                daftarGerbong[i] = gerbong;
+		success = true;
+                break;
+            }
 	}
-	
-	public Gerbong getGerbong(String id) { 
-		for(int i = 0; i < kapasitasGerbong; i++){
-			if(daftarGerbong[i].getIdGerbong().equals(id)){
-				return daftarGerbong[i];
-			}	
-		}
-		return null;
+	if(!success){ 
+            System.out.println("Failed addGerbong!");
+	} else jumlahGerbong++;
+    }
+    public void removeGerbong(String id){
+        boolean success = false;
+        for(int i = 0; i < kapasitasGerbong; i++){
+            if(daftarGerbong[i].getIdGerbong().equals(id)){
+                daftarGerbong[i] = null;
+		success = true;
+		break;
+            }
 	}
-	
-	public Rute getRuteKereta(String id) { 
-		for(int i = 0; i < kapasitasRute; i++){
-			if(daftarRute[i].getIdRute().equals(id)){
-				return daftarRute[i];
-			}
-		}
-		return null; 
-	}
-	
-	public int getKapasitasGerbong() { 
-		return kapasitasGerbong; 
-	}
-	
-	public int getKapasitasRute() { 
-		return kapasitasRute; 
-	}
-	
-	public int getJumlahGerbong() { 
-		return jumlahGerbong;
-	}
-	
-	public int getJumlahRute() { 
-		return jumlahRute;
-	}
-	
-	//Setter
-	public void setIdKereta(String id) {
-		idKereta = id; 
-	}
-	
-	public void setNamaKereta(String nama) {
-		namaKereta = nama; 
-	}
-	
-	public void addGerbong(Gerbong gerbong) { 
-		boolean success = false;
-		for(int i = 0; i < kapasitasGerbong; i++)
-		{
-			if(daftarGerbong[i] == null)
-			{
-				daftarGerbong[i] = gerbong;
-				success = true;
-				break;
-			}
-		}
-		if(!success){ 
-			System.out.println("Failed addGerbong!");
-		} else jumlahGerbong++;
-	}
-	
-	public void removeGerbong(String id){
-		boolean success = false;
-		for(int i = 0; i < kapasitasGerbong; i++){
-			if(daftarGerbong[i].getIdGerbong().equals(id)){
-				daftarGerbong[i] = null;
-				success = true;
-				break;
-			}
-		}
-		if(!success){ 
-			System.out.println("ID not found!");
-		} else jumlahGerbong--;
-	}
-	
-	public void addRute(Rute rute) { 
-		boolean success = false;
-		for(int i = 0; i < kapasitasRute; i++){
-			if(daftarRute[i] == null){
-				daftarRute[i] = rute;
-				success = true;
-				break;
-			}
-		}
-		if(!success){ 
-			System.out.println("Failed addRute!");		} else jumlahRute++;
-	}
-	
-	public void removeRute(String id){
-		boolean success = false;
-		for(int i = 0; i < kapasitasRute; i++){
-			if(daftarRute[i].getIdRute().equals(id)){
-				daftarRute[i] = null;
-				success = true;
-				break;
-			}
-		}
-		if(!success){ 
-			System.out.println("ID not found!"); 
-		} else jumlahRute--;
-	}
+	if(!success){ 
+            System.out.println("ID not found!");
+	} else jumlahGerbong--;
+    }
 }
