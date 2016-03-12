5a8d5c85ae620ce67e6e6e99ba6840869c05295e
hizas <hizassr@gmail.com>
2016-03-12 19:32:52 +0700

    Initialize Class

--- TubesPBO/src/Rute.java
+++ TubesPBO/src/Rute.java
@@ -1,3 +1,22 @@
 public class Rute {
+    private Stasiun awal;
+    private Stasiun tujuan;
+    private float distance;
     
+    public Rute(Stasiun awal, Stasiun tujuan, float distance)
+    {
+        this.awal = awal;
+        this.tujuan = tujuan;
+        this.distance = distance;
+    }
+
+    //Getter
+    public Stasiun getStasiunAwal() { return awal; }
+    public Stasiun getStasiunTujuan() { return tujuan; }
+    public float getDistance() { return distance; }
+    
+    //Setter
+    public void setStasiunAwal(Stasiun awal) { this.awal = awal; }
+    public void setStasiunTujuan(Stasiun tujuan) { this.tujuan = tujuan; }
+    public void setDistance(float distance) { this.distance = distance; }
 }
